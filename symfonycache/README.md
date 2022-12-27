43766352b91ccb402ac3e7e5c57b6e15740ea276
Jean-Claude <jeanyao@ymail.com>
2022-12-27 23:39:18 +0300

    Caching via Filesystem

---
+++ symfonycache/README.md
@@ -0,0 +1,74 @@
+### SYMFONY CACHE
+
+```
+// e.g http://localhost:8000/stock/amzn
+// e.g http://localhost:8000/stock/tsla
+// e.g http://localhost:8000/stock/shop
+// e.g http://localhost:8000/stock/aapl
+
+
+$ php bin/console debug:autowiring cache
+
+Autowirable Types
+=================
+
+ The following classes & interfaces can be used as type-hints when autowiring:
+ (only showing classes/interfaces matching cache)
+ 
+ CacheItemPoolInterface generates CacheItemInterface objects.
+ Psr\Cache\CacheItemPoolInterface (cache.app)
+ 
+ Interface for adapters managing instances of Symfony's CacheItem.
+ Symfony\Component\Cache\Adapter\AdapterInterface (cache.app) - deprecated
+ 
+ Interface implemented by HTTP cache stores.
+ Symfony\Component\HttpKernel\HttpCache\StoreInterface (http_cache.store)
+ 
+ Covers most simple to advanced caching needs.
+ Symfony\Contracts\Cache\CacheInterface (cache.app)
+ 
+ Allows invalidating cached items using tags.
+ Symfony\Contracts\Cache\TagAwareCacheInterface (cache.app.taggable
+ 
+
+================================= CACHE CONFIG ===================
+ ./config/packages/cache.yaml
+ framework:
+    cache:
+        # Unique name of your app: used to compute stable namespaces for cache keys.
+        #prefix_seed: your_vendor_name/app_name
+
+        # The "app" cache stores to the filesystem by default.
+        # The data in this cache should persist between deploys.
+        # Other options include:
+
+        # Redis
+        #app: cache.adapter.redis
+        #default_redis_provider: redis://localhost
+
+        # APCu (not recommended with heavy random-write workloads as memory fragmentation can cause perf issues)
+        #app: cache.adapter.apcu
+
+        # Namespaced pools use the above "app" backend by default
+        pools:
+            #my.dedicated.cache: null
+            # this mean, I can inject $stocksCache from anywhere and use it (in this case used cache.adapter.filesystem)
+            stocks_cache:
+                adapter: cache.adapter.filesystem
+
+ 
+ ===================== DELETE CACHE FROM POOL =======================
+ $ php bin/console cache:pool:delete stocks_cache <nameOfCache>
+ 
+ $ php bin/console cache:pool:delete stocks_cache AAPL
+ $ php bin/console cache:pool:delete stocks_cache SHOP
+ $ php bin/console cache:pool:delete stocks_cache AMZN
+ $ php bin/console cache:pool:delete stocks_cache TLSA
+ 
+ 
+===================== CLEAR CACHES FROM POOL =======================
+ $ php bin/console cache:pool:clear <tag.name>
+ $ php bin/console cache:pool:clear app.cache
+ $ php bin/console cache:pool:clear cache.app stocks_cache
+ 
+```
--- symfonycache/src/Controller/AppController.php
+++ symfonycache/src/Controller/AppController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\Cache\ItemInterface;
 
 
@@ -14,7 +15,7 @@ class AppController extends AbstractController
 {
 
        #[Route('/stock/{symbol}', name: 'get-stock', methods: ['GET'])]
-       public function stock($symbol, EntityManagerInterface $entityManager)
+       public function stock($symbol, EntityManagerInterface $entityManager, CacheInterface $cache)
        {
               // e.g http://localhost:8000/stock/amzn
               // e.g http://localhost:8000/stock/tsla
@@ -23,7 +24,7 @@ class AppController extends AbstractController
 
               $symbol = strtoupper($symbol);
 
-              $cache = new FilesystemAdapter();
+              /* $cache = new FilesystemAdapter(); */
 
               /** @var Stock $stock */
               $stock = $cache->get($symbol, function (ItemInterface $item) use ($symbol, $entityManager){
@@ -52,10 +53,9 @@ class AppController extends AbstractController
 
 
 
-
-    #[Route('/stock-demo-1/{symbol}', name: 'get-stock-demo-1', methods: ['GET'])]
-    public function stockDemo($symbol, EntityManagerInterface $entityManager)
-    {
+     #[Route('/stock-via-pools-cache-config/{symbol}', name: 'get-stock-via-pools-cache-config', methods: ['GET'])]
+      public function stockViaPoolsFromCacheConfiguration($symbol, EntityManagerInterface $entityManager, CacheInterface $stocksCache)
+      {
         // e.g http://localhost:8000/stock/amzn
         // e.g http://localhost:8000/stock/tsla
         // e.g http://localhost:8000/stock/shop
@@ -63,12 +63,16 @@ class AppController extends AbstractController
 
         $symbol = strtoupper($symbol);
 
-        $cache = new FilesystemAdapter();
+        /* $cache = new FilesystemAdapter(); */
 
         /** @var Stock $stock */
         $stock = $cache->get($symbol, function (ItemInterface $item) use ($symbol, $entityManager){
 
             echo 'Miss<br>';
+
+            $item->expiresAt(date_create('tomorrow')); // midnight
+
+            // $item->expiresAfter(3600); // 1 hour
 
             $repo = $entityManager->getRepository(Stock::class);
 
@@ -76,9 +80,78 @@ class AppController extends AbstractController
 
         });
 
-        // $cache->delete($symbol);
+        /*
+        $cache->delete($symbol);
         $cache->clear();
+        */
 
         return new Response("{$stock->getShortName()} has a current value of {$stock->getPrice()}");
     }
+
+      #[Route('/stock-via-cache-interface/{symbol}', name: 'get-stock-via-cache-interface', methods: ['GET'])]
+      public function stockViaCacheInterface($symbol, EntityManagerInterface $entityManager, CacheInterface $cache)
+      {
+        // e.g http://localhost:8000/stock/amzn
+        // e.g http://localhost:8000/stock/tsla
+        // e.g http://localhost:8000/stock/shop
+        // e.g http://localhost:8000/stock/aapl
+
+        $symbol = strtoupper($symbol);
+
+        /* $cache = new FilesystemAdapter(); */
+
+        /** @var Stock $stock */
+        $stock = $cache->get($symbol, function (ItemInterface $item) use ($symbol, $entityManager){
+
+            echo 'Miss<br>';
+
+            $item->expiresAt(date_create('tomorrow')); // midnight
+
+            // $item->expiresAfter(3600); // 1 hour
+
+            $repo = $entityManager->getRepository(Stock::class);
+
+            return $repo->findOneBy(['symbol' => $symbol]);
+
+        });
+
+        /*
+        $cache->delete($symbol);
+        $cache->clear();
+        */
+
+        return new Response("{$stock->getShortName()} has a current value of {$stock->getPrice()}");
+     }
+
+
+
+
+       #[Route('/stock-via-fs-adapter/{symbol}', name: 'get-stock-fs-adapter', methods: ['GET'])]
+        public function stockViaFileystemAdapterCache($symbol, EntityManagerInterface $entityManager)
+        {
+            // e.g http://localhost:8000/stock/amzn
+            // e.g http://localhost:8000/stock/tsla
+            // e.g http://localhost:8000/stock/shop
+            // e.g http://localhost:8000/stock/aapl
+
+            $symbol = strtoupper($symbol);
+
+            $cache = new FilesystemAdapter();
+
+            /** @var Stock $stock */
+            $stock = $cache->get($symbol, function (ItemInterface $item) use ($symbol, $entityManager){
+
+                echo 'Miss<br>';
+
+                $repo = $entityManager->getRepository(Stock::class);
+
+                return $repo->findOneBy(['symbol' => $symbol]);
+
+            });
+
+            // $cache->delete($symbol);
+            $cache->clear();
+
+            return new Response("{$stock->getShortName()} has a current value of {$stock->getPrice()}");
+        }
 }
