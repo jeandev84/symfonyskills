4765d62defa30c6617f9be90451d9f03c5812302
Jean-Claude <jeanyao@ymail.com>
2022-12-30 15:29:44 +0300

    Object create

--- oopsoftwaredev/chapter1/creating-classes.php
+++ oopsoftwaredev/chapter1/creating-classes.php
@@ -1,40 +1,26 @@
 <?php
 
-
-/* Create a Class */
-/*
-class Product
-{
-
-}
-
-
-$productInstance = new Product();
-$anotherProductInstance = new Product();
-var_dump($productInstance);
-var_dump($anotherProductInstance);
-*/
-
-
-/* Create properties classes */
 class Product
 {
     public $name = 'soap';
-    public $price = 10;
+    public $price; # cens
+
+
+    public function priceAsCurrency()
+    {
+         $priceAsCurrency = $this->price / 100;
+
+         return $priceAsCurrency;
+    }
 }
 
 
-$soapObject = new Product();
-print $soapObject->name ."\n";
-$soapObject->name = 'toothpaste';
-print $soapObject->name ."\n";
+$soapObject1 = new Product();
+$soapObject1->price = 550;
 
-# Challenge
-# 1. Create a price property and given it a default integer value
-# 2. Access the price on a product object and print it
-$product = new Product();
-print $product->price . "\n";
-# 3. Change the price
-$product->price = 5;
-# Access the (new) price on a product object and print it.
-print $product->price . "\n";
+$soapObject2 = new Product();
+$soapObject2->price = 650;
+
+$priceAsCurrency = $soapObject1->priceAsCurrency();
+var_dump($priceAsCurrency) . PHP_EOL;
+
