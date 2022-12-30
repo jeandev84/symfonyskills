83a699e49019f80e910b6ed436f391a3a7db9ec4
Jean-Claude <jeanyao@ymail.com>
2022-12-31 01:43:45 +0300

    Properties and statics

--- oopsoftwaredev/oop2/chapter3/Calendar/Calendar.php
+++ oopsoftwaredev/oop2/chapter3/Calendar/Calendar.php
@@ -40,22 +40,3 @@ class Calendar
 }
 
 
-$calendar = new Calendar();
-$calendar->name = 'Year Planner';
-
-$calendar2 = new Calendar();
-print Calendar::$count;
-
-print $calendar->name . PHP_EOL;
-print $calendar->getMonthsInYear() . PHP_EOL;
-print Calendar::MONTHS_IN_YEAR;
-
-
-print Calendar::$daysInFebulary . PHP_EOL;
-Calendar::$daysInFebulary++;
-print $calendar::$daysInFebulary . PHP_EOL;
-
-
-print_r($calendar->seasons);
-print_r($calendar->weeksInYear);
-print $calendar->year. PHP_EOL;
