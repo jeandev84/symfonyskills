9cd547ae52cc02d60475a8d0a28b1026bd76016d
Jean-Claude <jeanyao@ymail.com>
2022-12-30 19:01:11 +0300

    Create the diffents type of Books

--- oopsoftwaredev/chapter2/inheritance.php
+++ oopsoftwaredev/chapter2/inheritance.php
@@ -4,7 +4,7 @@ require_once 'PhysicalBook.php';
 require_once 'DigitalBook.php';
 
 
-$physicalBook = new PhysicalBook('A Random Book', 'Jane Doe', 2000, 35);
+$physicalBook = new PhysicalBook('A Random Book', 'Jane Doe', 2000, 300);
 
 print $physicalBook->getAuthor() . PHP_EOL;
 print $physicalBook->print() . PHP_EOL;
