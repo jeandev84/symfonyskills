f72be149145044d29e92d3123a9f20272f3fe42c
Jean-Claude <jeanyao@ymail.com>
2022-12-31 04:02:44 +0300

    Abstract classes and Abstract methods

---
+++ oopsoftwaredev/oop2/chapter3/Shape/NonCuboidShape.php
@@ -0,0 +1,7 @@
+<?php
+require_once __DIR__.'/ThreeDimensionalShape.php';
+
+
+abstract class NonCuboidShape extends ThreeDimensionalShape
+{
+}
--- oopsoftwaredev/oop2/chapter3/abstract-methods.php
+++ oopsoftwaredev/oop2/chapter3/abstract-methods.php
@@ -11,11 +11,19 @@
 <?php
 
     require_once __DIR__.'/Shape/Types/Cylinder.php';
+    require_once __DIR__.'/Shape/Types/Sphere.php';
 
     $cylinder = new Cylinder(['radius' => 5, 'height' => 10]);
     $cylinderVolume = $cylinder->volume();
 
-    print_r($cylinderVolume);
+    /* print_r($cylinderVolume); */
+
+    $sphere = new Sphere(['radius' => 5]);
+    $sphereVolume = $sphere->volume();
+
+    print_r($sphereVolume);
+
+
 ?>
 
 </body>
