e18596a79c3bbe6f587bf6ad366fab2a4b6f4eb6
Jean-Claude <jeanyao@ymail.com>
2023-01-02 11:31:33 +0300

    Start Project BTB

--- sessions/btb-project/tests/SessionTest.php
+++ sessions/btb-project/tests/SessionTest.php
@@ -5,6 +5,14 @@ class SessionTest extends \PHPUnit\Framework\TestCase
     /** @test */
     public function can_check_if_a_session_is_started()
     {
-         $this->assertTrue(true);
+         /* $this->assertTrue(true); */
+
+         // SETUP
+         // Create a session object
+         $session = new \App\Session\Session();
+
+
+         // Assert session is not started
+         $this->assertFalse($session->isStarted());
     }
 }
