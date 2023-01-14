d9e2a908ef83a8bae8eaba0e14e198cb0b658648
Jean-Claude <jeanyao@ymail.com>
2023-01-14 16:37:49 +0300

    about DOM javascript

--- javascripts/dom/assets/js/app.js
+++ javascripts/dom/assets/js/app.js
@@ -1,64 +1,13 @@
-/**
- * Useful functions
+/*
+console.log(window)
+console.log(window.document)
+console.log(window.document.getElementById("elementId"));
+console.log(window.document.querySelector("#elementId"));
 */
 
-function jsQuery(anchor) {
-    return document.querySelector(anchor);
-}
+let newParagraph = document.createElement('p');
+let pContent     = document.createTextNode('I am new paragraph');
 
-function jsElementsByName(name) {
-    return document.getElementsByName(name);
-}
-
-function jsElementById(id) {
-    return document.getElementById(id);
-}
-
-function jsElementByClassname(classname) {
-    return document.getElementsByClassName(classname);
-}
-
-
-
-/**
- * Collecting form data
- * How to collect data from text boxes
-*/
-
-
-let firstname = jsElementById("firstname").value;
-let lastname  = jsElementById("lastname").value;
-let email     = jsElementById("email").value;
-
-
-/**
- * Getting selected value from a dropdown list
-*/
-
-let country  = jsQuery('#country').value;
-
-
-/**
- * Getting value of checked radio button
-*/
-
-let gender = jsQuery('input[name="gender"]:checked').value;
-
-
-
-/**
- * Getting all selecting checked checkboxes
-*/
-
-let hobbies = [];
-let checkboxes = jsElementsByName('hobbies[]');
-
-for(let i = 0; i < checkboxes.length; i++) {
-    if (checkboxes[i].checked) {
-        hobbies.push(checkboxes[i].value);
-    }
-}
-
-console.log(hobbies);
-
+newParagraph.appendChild(pContent);
+document.body.appendChild(newParagraph);
 
