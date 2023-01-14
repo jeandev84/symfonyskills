72ab8b7f74a958fb9d74fd75f431c8709675fa66
Jean-Claude <jeanyao@ymail.com>
2023-01-14 23:41:15 +0300

    Pig Game implementation

--- javascripts/dom/assets/js/app.js
+++ javascripts/dom/assets/js/app.js
@@ -1,18 +1,48 @@
 /**
- * Get ACCESS TO GLOBAL SCORE ELEMENTS
+ * Get access to webpage elements
 */
 
-let score0 = document.getElementById('score--0');
-let score1 = document.getElementById('score--1');
-let dice   = document.querySelector('.dice')
-
+let score0          = document.getElementById('score--0');
+let score1          = document.getElementById('score--1');
+let diceImage       = document.querySelector('.dice')
+let rollDiceBtn     = document.querySelector('.btn--roll');
 
 /**
  * Initialize the values to zero
 */
 score0.textContent = 0;
 score1.textContent = 0;
-dice.classList.add('hidden');
+
+
+/**
+ * Hide dice
+*/
+diceImage.classList.add('hidden');
+
+
+/**
+ * Implement functionality for roll dice button
+*/
+rollDiceBtn.addEventListener('click', function () {
+
+    // Math.random(); [0...10000..]
+    // Math.trunc(Math.random() * 6) Generate [0-6]
+
+    // 1. Generate a random number between : 1 and 6
+    let diceRandomNumber = Math.trunc(Math.random() * 6) + 1;
+
+
+    // 2. Display the dice image with the random number
+    diceImage.classList.remove('hidden');
+
+
+
+    // 3. If the random number is not 1 then add it to active player current score
+
+
+    // 4. If random is 1 then reset current score to zero and change the active player
+
+});
 
 
 
