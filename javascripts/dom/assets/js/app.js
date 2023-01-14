3a83fef1a18d203351b2a09d4113e11cf4d63a3c
Jean-Claude <jeanyao@ymail.com>
2023-01-14 17:39:58 +0300

    object manipulate

--- javascripts/dom/assets/js/app.js
+++ javascripts/dom/assets/js/app.js
@@ -11,12 +11,45 @@ console.log(window.document)
 console.log(window.document.getElementById("elementId"));
 console.log(window.document.querySelector("#elementId"));
 
+===================================================================================================
 # Create a new element e.g : paragraph
+===================================================================================================
+
 let newParagraph = document.createElement('p');
 let pContent     = document.createTextNode('I am new paragraph');
 
 newParagraph.appendChild(pContent);
 document.body.appendChild(newParagraph);
+
+=================================================================================================
+# Object manipulate
+=================================================================================================
+let p = document.getElementById('intro');
+console.log(p)
+
+
+let obj = {
+    name: "",
+    username: "",
+    getFullName: function() {
+       return this.name + this.username;
+    }
+}
+
+obj.name = 'Jean-Claude'
+console.log(obj.name)
+
+let formObj = {
+    payload: {},
+    setAttributes: function(payload) {
+       this.payload = payload
+    },
+    send: function (form) {
+         return "form sent ..." + form;
+    }
+}
+
+console.log(formObj.send(1))
 */
 
 
@@ -43,13 +76,58 @@ obj.name = 'Jean-Claude'
 console.log(obj.name)
 
 let formObj = {
-    sendForm: function (form) {
+    payload: {},
+    setAttributes: function(payload) {
+       this.payload = payload
+    },
+    send: function (form) {
          return "form sent ..." + form;
     }
 }
 
+console.log(formObj.send(1))
 
-console.log(formObj.sendForm(1))
+=====================================================================
+console.log('GET ELEMENT BY ID')
+console.log('===============================================')
+let p = document.getElementById('intro');
+console.log(p)
+console.log('===============================================')
+console.log('GET ELEMENT BY QUERY SELECTOR')
+console.log('===============================================')
+let div = document.querySelector('.list');
+console.log(div)
 */
 
 
+function printHeader(title) {
+    console.log('START LOG ['+ title +']')
+    console.log('===============================================')
+}
+
+
+function printFooter() {
+    console.log('END LOG')
+    console.log('===============================================')
+    console.log('\n')
+}
+
+function consoleLog(element, title = 'ELEMENT') {
+    printHeader(title)
+    console.log(element);
+    printFooter()
+}
+
+
+/**
+ * HtmlElement
+ *
+ * @type {HTMLElement}
+*/
+
+
+let p = document.getElementById('intro');
+consoleLog(p, 'GET ELEMENT BY ID');
+
+let div = document.querySelector('.list');
+consoleLog(div, 'GET ELEMENT BY QUERY SELECTOR')
