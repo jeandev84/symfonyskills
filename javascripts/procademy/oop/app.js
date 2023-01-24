d6a4282720d4fc1f99f5c9aaea5a0e2a33386e2e
Jean-Claude <jeanyao@ymail.com>
2023-01-25 00:18:57 +0300

    Inheritance between Classes in JavaScript

--- javascripts/procademy/oop/app.js
+++ javascripts/procademy/oop/app.js
@@ -1,63 +1,29 @@
-// Inheritance between function constructors in Javascript
-// Person function constructor
-// Person - parent class (base class)
-let Person = function (name, gender, birthYear) {
-    this.name = name;
-    this.gender = gender;
-    this.birthYear = birthYear;
+// Inheritance between classes
+class Person {
+
+    constructor(name, birthYear, gender) {
+        this.name = name;
+        this.birthYear = birthYear;
+        this.gender = gender;
+    }
+
+    calculateAge() {
+        let age = new Date().getFullYear() - this.birthYear;
+        console.log(age)
+    }
 }
 
 
-Person.prototype.calculateAge = function () {
-    let age = new Date().getFullYear() - this.birthYear;
-    console.log(age)
+
+class Employee extends Person {
+
+    constructor(name, gender, birthYear, employeeId, salary) {
+        super(name, gender, birthYear);
+        this.employeeId = employeeId;
+        this.salary     = salary;
+    }
 }
 
 
-let john = new Person('John', 'Male', 1990);
-console.log(john);
-
-
-// Employee function constructor
-/*
-DON'T REPEAT YOUR SELF (DRY PRINCIPLE)
-let Employee = function (name, gender, birthYear, employeeId, salary) {
-    this.name = name;
-    this.gender = gender;
-    this.birthYear = birthYear;
-    this.employeeId = employeeId;
-    this.salary = salary;
-}
-*/
-
-
-// Employee function constructor
-// this = mark
-// Employee - child class
-let Employee = function (name, gender, birthYear, employeeId, salary) {
-    Person.call(this, name, gender, birthYear);
-    this.employeeId = employeeId;
-    this.salary = salary;
-}
-
-
-// Inheritance all methods of Parent class (Assign prototype Child and Parent before running child prototype)
-Employee.prototype = Person.prototype;
-
-
-
-// And then
-Employee.prototype.calculateSalary = function () {
-    return this.salary * 12;
-}
-
-
-Employee.prototype.employeeDetails = function () {
-    console.log(this.name);
-    console.log(this.employeeId);
-}
-
-
-let mark = new Employee('Mark', 'Male', 1995, 101, 12000);
+let mark = new Employee('Mark', 'Male', 1995, 201, 18000);
 console.log(mark);
-
