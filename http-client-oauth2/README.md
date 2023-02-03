acef445a4f36929f4dd7029f1fb69c589e183678
Jean-Claude <jeanyao@ymail.com>
2023-02-04 00:58:36 +0300

    OAuth2 - Open Authorization connect from A to B application

---
+++ http-client-oauth2/README.md
@@ -0,0 +1,15 @@
+### HTTP CLIENT 
+
+- https://symfony.com/blog/new-in-symfony-4-4-httpclient-component
+- https://developer.github.com/v3/repos/#list-user-repositories | https://docs.github.com/en/rest/repos?apiVersion=2022-11-28#list-user-repositories
+
+1. Create project
+```
+$  composer create-project symfony/website-skeleton:"4.4.*" http-client-sandox
+```
+
+
+2. Tasks 
+```
+Recuperer tous les users et les afficher
+```
---
+++ http-client-oauth2/src/Controller/v1/ApiController.php
@@ -0,0 +1,99 @@
+<?php
+namespace App\Controller\v1;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+
+/**
+ * @Route("/api/v1", name="api.v1.")
+*/
+class ApiController extends AbstractController
+{
+    /**
+     * @Route("/", name="list")
+     * @param HttpClientInterface $client
+     * @return Response
+     * @throws TransportExceptionInterface
+     * @throws ClientExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws DecodingExceptionInterface
+    */
+    public function list(HttpClientInterface $client): Response
+    {
+        $response = $client->request('GET', 'https://api.github.com/users/jeandev84/repos', [
+            'query' => [
+                'sort' => 'created', // sort by createdAt
+            ]
+        ]);
+
+        // dd($response->getContent());
+        // dd($response->toArray());
+
+        return $this->render('api/v1/list.html.twig', [
+            'repositories' => $response->toArray()
+        ]);
+    }
+
+
+    /**
+     * @Route("/show/{id}", name="show")
+     * @param $id
+     * @param HttpClientInterface $client
+     * @return Response
+     * @throws ClientExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws TransportExceptionInterface
+    */
+    public function show($id, HttpClientInterface $client): Response
+    {
+        $response = $client->request('GET', "https://api.github.com/repositories/{$id}");
+
+        if ($response->getStatusCode() === Response::HTTP_NOT_FOUND) {
+             throw new NotFoundHttpException(sprintf('No repository with id %s', $id));
+        }
+
+        return $this->render('api/v1/show.html.twig', [
+            'repository' => $response->toArray()
+        ]);
+    }
+
+    /**
+     * @Route("/create", name="app_api")
+     * @param HttpClientInterface $client
+     * @return Response
+     * @throws TransportExceptionInterface
+     * @throws ClientExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws DecodingExceptionInterface
+    */
+    public function create(HttpClientInterface $client): Response
+    {
+        $response = $client->request('GET', 'https://api.github.com/users/jeandev84/repos', [
+            'query' => [
+                'sort' => 'created', // sort by createdAt
+            ]
+        ]);
+
+        // dd($response->getContent());
+        // dd($response->toArray());
+
+        return $this->render('api/index.html.twig', [
+            'controller_name' => 'ApiController',
+            'users' => $response->toArray()
+        ]);
+    }
+}
