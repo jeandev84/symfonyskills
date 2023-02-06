a471ada02c75360edbee1020e0b94379e0804e27
Jean-Claude <jeanyao@ymail.com>
2023-02-07 00:17:03 +0300

    Implemented OA

--- apisymfony5/src/Controller/Api/v1/BookController.php
+++ apisymfony5/src/Controller/Api/v1/BookController.php
@@ -1,43 +1,38 @@
 <?php
+
 namespace App\Controller\Api\v1;
 
+use App\DTO\Model\Book\BookListResponse;
 use App\Exception\Book\BookCategoryNotFoundException;
 use App\Service\Book\BookService;
-use App\DTO\Model\Book\BookListResponse;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use OpenApi\Annotations as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Annotation\Route;
-use Nelmio\ApiDocBundle\Annotation\Model;
-use OpenApi\Annotations as OA;
-
 
 class BookController extends AbstractController
 {
-       public function __construct(protected BookService $bookService)
-       {
-       }
+    public function __construct(protected BookService $bookService)
+    {
+    }
 
-
-       /**
-        * @OA\Response(
-        *     response=200,
-        *     description="Returns books inside a category",
-        *     @Model(type=BookListResponse::class)
-        * )
-        * @param int $id
-        * @return JsonResponse
-        * @throws HttpException
-       */
-       #[Route(path: '/api/v1/category/{id}/books', name: 'api.v1.category.books', methods: ['GET'])]
-       public function getBooksByCategory(int $id):JsonResponse
+    /**
+     * @OA\Response(
+     *     response=200,
+     *     description="Returns books inside a category",
+     *     @Model(type=BookListResponse::class)
+     * )
+     *
+     * @throws HttpException
+     */
+    #[Route(path: '/api/v1/category/{id}/books', name: 'api.v1.category.books', methods: ['GET'])]
+       public function getBooksByCategory(int $id): JsonResponse
        {
            try {
-
                return $this->json($this->bookService->getBooksByCategory($id));
-
            } catch (BookCategoryNotFoundException $exception) {
-
                throw new HttpException($exception->getCode(), $exception->getMessage());
            }
        }
--- apisymfony5/tests/Controller/Api/v1/BookCategoryControllerTest.php
+++ apisymfony5/tests/Controller/Api/v1/BookCategoryControllerTest.php
@@ -2,25 +2,22 @@
 
 namespace App\Tests\Controller\Api\v1;
 
-use App\Controller\Api\v1\BookCategoryController;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
 
 // Functional Test
 class BookCategoryControllerTest extends WebTestCase
 {
-
     public function testListBookCategories(): void
     {
-          $client = static::createClient();
-          $client->request('GET', '/api/v1/book/categories');
-          $responseContent = $client->getResponse()->getContent();
+        $client = static::createClient();
+        $client->request('GET', '/api/v1/book/categories');
+        $responseContent = $client->getResponse()->getContent();
 
-          # If response code is success
-          $this->assertResponseIsSuccessful();
-          $this->assertJsonStringEqualsJsonFile(
-   __DIR__.'/responses/BookCategoryControllerTest_testListBookCategories.json',
-              $responseContent
-          );
+        // If response code is success
+        $this->assertResponseIsSuccessful();
+        $this->assertJsonStringEqualsJsonFile(
+            __DIR__.'/responses/BookCategoryControllerTest_testListBookCategories.json',
+            $responseContent
+        );
     }
 }
