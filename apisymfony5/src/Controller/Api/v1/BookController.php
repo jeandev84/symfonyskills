32864846fe348ba1e46ed22a925f5d05e7c6dff7
Jean-Claude <jeanyao@ymail.com>
2023-02-08 08:03:47 +0300

    Exception

--- apisymfony5/src/Controller/Api/v1/BookController.php
+++ apisymfony5/src/Controller/Api/v1/BookController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Api\v1;
 
 use App\DTO\Model\Book\BookListResponse;
+use App\DTO\Model\Exception\Response\ErrorResponse;
 use App\Service\Book\BookService;
 use Nelmio\ApiDocBundle\Annotation\Model;
 use OpenApi\Annotations as OA;
@@ -21,6 +22,11 @@ class BookController extends AbstractController
      *     response=200,
      *     description="Returns books inside a category",
      *     @Model(type=BookListResponse::class)
+     * )
+     * @OA\Response(
+     *     response=404,
+     *     description="Book category not found",
+     *     @Model(type=ErrorResponse::class)
      * )
      */
     #[Route(path: '/api/v1/category/{id}/books', name: 'api.v1.category.books', methods: ['GET'])]
