876c2028e10962648bad9d8b14374f10af84fb29
Jean-Claude <jeanyao@ymail.com>
2023-02-09 20:10:29 +0300

    Refactoring

--- apisymfony5/tests/Listener/ApiExceptionListenerTest.php
+++ apisymfony5/tests/Listener/ApiExceptionListenerTest.php
@@ -19,6 +19,21 @@ use Symfony\Component\Serializer\SerializerInterface;
 
 class ApiExceptionListenerTest extends AbstractTestCase
 {
+    private ExceptionMappingResolver $resolver;
+
+    private LoggerInterface $logger;
+
+    private SerializerInterface $serializer;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->resolver = $this->createMock(ExceptionMappingResolver::class);
+        $this->logger = $this->createMock(LoggerInterface::class);
+        $this->serializer = $this->createMock(SerializerInterface::class);
+    }
+
     public function testNon500MappingWithHiddenMessage(): void
     {
         $mapping = ExceptionMapping::fromCode(Response::HTTP_NOT_FOUND);
@@ -27,30 +42,35 @@ class ApiExceptionListenerTest extends AbstractTestCase
 
         $responseBody = json_encode(['error' => $responseMessage]);
 
-        $resolver = $this->createMock(ExceptionMappingResolver::class);
-        $resolver->expects($this->once())
+        $this->resolver->expects($this->once())
                  ->method('resolve')
                  ->will(InvalidArgumentException::class)
                  ->willReturn($mapping);
 
-        $logger = $this->createMock(LoggerInterface::class);
-
-        $serializer = $this->createMock(SerializerInterface::class);
-        $serializer->expects($this->once())
+        $this->serializer->expects($this->once())
                    ->method('serialize')
                    ->will(new ErrorResponse($responseMessage), JsonEncoder::FORMAT)
                    ->willReturn($responseBody);
 
         $event = $this->createEvent(new InvalidArgumentException('test'));
 
-        $listener = new ApiExceptionListener($resolver, $logger, $serializer, false);
-        $listener($event);
+        $this->runListener($event);
 
-        $response = $event->getResponse();
+        $this->assertResponse(Response::HTTP_NOT_FOUND, $responseBody, $event->getResponse());
+    }
 
-        $this->assertEquals(Response::HTTP_NOT_FOUND, $response->getStatusCode());
-        $this->assertInstanceOf(JsonResponse::class, $response);
-        $this->assertJsonStringEqualsJsonFile($responseBody, $response->getContent());
+
+
+    private function assertResponse(int $expectedStatusCode, string $expectedBody, Response $actualResponse): void
+    {
+        $this->assertEquals($expectedStatusCode, $actualResponse->getStatusCode());
+        $this->assertInstanceOf(JsonResponse::class, $actualResponse);
+        $this->assertJsonStringEqualsJsonFile($expectedBody, $actualResponse->getContent());
+    }
+
+    private function runListener(ExceptionEvent $event, bool $isDebug = false): void
+    {
+        (new ApiExceptionListener($this->resolver, $this->logger, $this->serializer, $isDebug))($event);
     }
 
     public function createEvent(InvalidArgumentException $e): ExceptionEvent
@@ -58,7 +78,7 @@ class ApiExceptionListenerTest extends AbstractTestCase
         return new ExceptionEvent(
             $this->createTestKernel(),
             new Request(),
-HttpKernelInterface::MAIN_REQUEST,
+            HttpKernelInterface::MAIN_REQUEST,
             $e
         );
     }
