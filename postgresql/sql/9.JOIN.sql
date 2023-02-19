cd1ef6212c78d1a61a79d1d062a107181a442707
Jean-Claude <jeanyao@ymail.com>
2023-02-19 05:02:00 +0300

    JOIN запрос данных из нескольких таблиц

--- postgresql/sql/9.JOIN.sql
+++ postgresql/sql/9.JOIN.sql
@@ -39,23 +39,66 @@ JOIN product_types ON products.type_id = product_types.id;
 
 SELECT p.name, t.type_name
 FROM  products AS p
-JOIN product_types AS t ON p.type_id = t.id;
+JOIN product_types AS t
+ON p.type_id = t.id;
 
 
 SELECT p.name, t.type_name
 FROM  products AS p
-JOIN product_types AS t ON p.type_id = t.id;
+JOIN product_types AS t
+ON p.type_id = t.id;
 
 
 
 
---- JOIN WITH USE PSEUDONYM AND WHERE (Filter)
+--- (Фильтрия) JOIN WITH USE PSEUDONYM AND WHERE
 
 SELECT
     p.name AS product_name,
-    t.type_name as product_type,
-    p.price as product_price
+    t.type_name As product_type,
+    p.price As product_price
 FROM  products AS p
-JOIN product_types AS t ON p.type_id = t.id
+JOIN product_types AS t
+ON p.type_id = t.id
 WHERE t.type_name = 'Онлайн-курс';
 
+
+
+SELECT
+    p.name AS product_name,
+    t.type_name As product_type,
+    p.price As product_price
+FROM  products AS p
+JOIN product_types AS t
+ON p.type_id = t.id
+WHERE t.type_name = 'Вебинар' AND p.price = 0;
+
+
+--- СОРТИРОВКА (JOIN)
+
+SELECT
+    p.name AS product_name,
+    t.type_name As product_type,
+    p.price As product_price
+FROM  products AS p
+JOIN product_types AS t
+ON p.type_id = t.id
+WHERE t.type_name = 'Онлайн-курс'
+ORDER BY p.price DESC;
+
+
+
+--- ИТОГИ
+--- Запрос данных из несколько таблиц
+--- Оператор SELECT
+--- В ключевом слове FROM указываем несколько таблиц с JOIN
+--- После ключевого слова ON указываем условия объединения
+
+
+--- Связи между таблицами
+--- Ссылки из одной таблицы на другую
+--- Внешний ключ ( FOREIGN KEY )
+
+--- Использование JOIN в SELECT
+--- Комбинация с WHERE, ORDER BY, LIMIT, GROUP BY, HAVING
+--- Типы JOIN. внешнее и внутреннее, левое и правое, перекрестное, полное.
